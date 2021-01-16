8cebe4557f81e44d39a34630b85b259d2c674812
Bastian Block <blockbasti99@gmail.com>
2021-01-16 12:29:18 +0100

    Add License Page

--- lib/SettingsPage.dart
+++ lib/SettingsPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:just_another_workout_timer/OssLicensesPage.dart';
 import 'package:just_another_workout_timer/TTSHelper.dart';
 import 'package:preferences/dropdown_preference.dart';
@@ -12,8 +13,18 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  String _license = '';
+
+  void _loadLicense() async {
+    var lic = await rootBundle.loadString('LICENSE');
+    setState(() {
+      _license = lic;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    _loadLicense();
     return Scaffold(
         appBar: AppBar(
           title: Text('Settings'),
@@ -46,7 +57,34 @@ class _SettingsPageState extends State<SettingsPage> {
           ),
           PreferenceTitle('Licenses'),
           PreferenceText(
-            'View Licenses',
+            'View License',
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return Dialog(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(8),
+                            child: Text(
+                              'Just Another Workout Timer',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(8),
+                            child: Text(_license),
+                          )
+                        ],
+                      ),
+                    );
+                  });
+            },
+          ),
+          PreferenceText(
+            'View Open Source Licenses',
             onTap: () {
               Navigator.push(
                   context,
