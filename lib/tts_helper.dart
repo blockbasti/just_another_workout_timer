292ce699ef3823057cd74344f4b97986be8e9e2c
Bastian Block <blockbasti99@gmail.com>
2021-03-07 17:43:17 +0100

    fix: disable TTS when no TTS engine is installed (fixes #36)

--- lib/tts_helper.dart
+++ lib/tts_helper.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:preferences/preference_service.dart';
 
@@ -6,7 +8,9 @@ import 'package:preferences/preference_service.dart';
 class TTSHelper {
   static FlutterTts flutterTts;
 
-  static List<String> languages;
+  static List<String> languages = List.empty();
+
+  static bool available = true;
 
   /// enable/disable TTS output
   static bool useTTS = true;
@@ -15,18 +19,32 @@ class TTSHelper {
     flutterTts = FlutterTts();
 
     useTTS = PrefService.getString('sound') == 'tts' ?? true;
-    await flutterTts.setLanguage(PrefService.getString('tts_lang') ?? 'en-US');
-    await flutterTts.setSpeechRate(1.0);
-    await flutterTts.setVolume(1.0);
-    await flutterTts.setPitch(1.0);
 
-    languages = List<String>.from(await flutterTts.getLanguages);
-    languages.sort();
-    languages.remove('de-DE');
-    languages.remove('en-US');
-    languages.insertAll(0, ['en-US', 'de-DE']);
-    languages = await Future.wait(languages.map((e) async =>
-        '$e${await flutterTts.isLanguageInstalled(e) ? '' : '*'}'));
+    var ttsLang = PrefService.getString('tts_lang') ?? 'en-US';
+    if (ttsLang.endsWith('*')) ttsLang = ttsLang.replaceAll('*', '');
+    PrefService.setString('tts_lang', ttsLang);
+
+    try {
+      await flutterTts
+          .setLanguage(ttsLang)
+          .timeout(Duration(seconds: 1))
+          .then((_) async {
+        await flutterTts.setSpeechRate(1.0);
+        await flutterTts.setVolume(1.0);
+        await flutterTts.setPitch(1.0);
+
+        languages = List<String>.from(await flutterTts.getLanguages);
+        languages.sort();
+        languages.remove('de-DE');
+        languages.remove('en-US');
+        languages.insertAll(0, ['en-US', 'de-DE']);
+      });
+    } on TimeoutException {
+      available = false;
+      useTTS = false;
+      PrefService.setString('sound', 'beep');
+      return;
+    }
   }
 
   static void speak(String text) async {
