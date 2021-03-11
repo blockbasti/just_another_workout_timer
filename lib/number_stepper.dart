4665d0f49c0a9f59b21185831ee40c109f0973be
Bastian Block <blockbasti99@gmail.com>
2021-03-11 19:48:41 +0100

    feat: allow changing exercise duration in 1 second steps (closes #38)

--- lib/number_stepper.dart
+++ lib/number_stepper.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 import 'utils.dart';
 
@@ -8,18 +9,18 @@ class NumberStepper extends StatefulWidget {
   NumberStepper(
       {@required this.lowerLimit,
       @required this.upperLimit,
-      @required this.stepValue,
       @required this.value,
       @required this.valueChanged,
-      @required this.formatNumber});
+      @required this.formatNumber,
+      @required this.largeSteps});
 
   final int lowerLimit;
   final int upperLimit;
-  final int stepValue;
   final double iconSize = 16;
   int value;
   final ValueChanged<int> valueChanged;
   final bool formatNumber;
+  final bool largeSteps;
 
   @override
   _CustomStepperState createState() => _CustomStepperState();
@@ -27,42 +28,132 @@ class NumberStepper extends StatefulWidget {
 
 class _CustomStepperState extends State<NumberStepper> {
   @override
-  Widget build(BuildContext context) => Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          IconButton(
-            icon: Icon(Icons.remove),
-            iconSize: widget.iconSize,
-            onPressed: () {
-              setState(() {
-                widget.value = widget.value == widget.lowerLimit
-                    ? widget.lowerLimit
-                    : widget.value -= widget.stepValue;
-              });
-              widget.valueChanged(widget.value);
-            },
-          ),
-          Container(
-            //width: widget.iconSize*2,
-            child: Text(
-              '${widget.formatNumber ? Utils.formatSeconds(widget.value) : widget.value}',
-              style: TextStyle(
-                  fontSize: widget.iconSize * 0.9, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
+  Widget build(BuildContext context) => widget.largeSteps
+      ? Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              width: 24,
+              child: FloatingActionButton(
+                heroTag: null,
+                child: Text(
+                  '-5',
+                  style: TextStyle(color: Colors.white),
+                ),
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                onPressed: () {
+                  setState(() {
+                    widget.value = widget.value == widget.lowerLimit
+                        ? widget.lowerLimit
+                        : widget.value -= 5;
+                  });
+                  widget.valueChanged(widget.value);
+                },
+              ),
             ),
-          ),
-          IconButton(
-            icon: Icon(Icons.add),
-            iconSize: widget.iconSize,
-            onPressed: () {
-              setState(() {
-                widget.value = widget.value == widget.upperLimit
-                    ? widget.upperLimit
-                    : widget.value += widget.stepValue;
-              });
-              widget.valueChanged(widget.value);
-            },
-          ),
-        ],
-      );
+            Container(
+              width: 24,
+              child: FloatingActionButton(
+                child: Text('-1', style: TextStyle(color: Colors.white)),
+                heroTag: null,
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                onPressed: () {
+                  setState(() {
+                    widget.value = widget.value == widget.lowerLimit
+                        ? widget.lowerLimit
+                        : widget.value -= 1;
+                  });
+                  widget.valueChanged(widget.value);
+                },
+              ),
+            ),
+            Container(
+              //width: widget.iconSize*2,
+              padding: EdgeInsets.symmetric(horizontal: 4),
+              child: Text(
+                '${widget.formatNumber ? Utils.formatSeconds(widget.value) : widget.value}',
+                style: TextStyle(
+                    fontSize: widget.iconSize * 0.9,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Container(
+              width: 24,
+              child: FloatingActionButton(
+                heroTag: null,
+                child: Text(
+                  '+1',
+                  style: TextStyle(color: Colors.white),
+                ),
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                onPressed: () {
+                  setState(() {
+                    widget.value = widget.value == widget.upperLimit
+                        ? widget.upperLimit
+                        : widget.value += 1;
+                  });
+                  widget.valueChanged(widget.value);
+                },
+              ),
+            ),
+            Container(
+              width: 24,
+              child: FloatingActionButton(
+                heroTag: null,
+                child: Text('+5', style: TextStyle(color: Colors.white)),
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                onPressed: () {
+                  setState(() {
+                    widget.value = widget.value == widget.upperLimit
+                        ? widget.upperLimit
+                        : widget.value += 5;
+                  });
+                  widget.valueChanged(widget.value);
+                },
+              ),
+            ),
+          ],
+        )
+      : Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              icon: Icon(Icons.remove),
+              onPressed: () {
+                setState(() {
+                  widget.value = widget.value == widget.lowerLimit
+                      ? widget.lowerLimit
+                      : widget.value -= 1;
+                });
+                widget.valueChanged(widget.value);
+              },
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 4),
+              child: Text(
+                '${widget.formatNumber ? Utils.formatSeconds(widget.value) : widget.value}',
+                style: TextStyle(
+                    fontSize: widget.iconSize * 0.9,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                setState(() {
+                  widget.value = widget.value == widget.upperLimit
+                      ? widget.upperLimit
+                      : widget.value += 1;
+                });
+                widget.valueChanged(widget.value);
+              },
+            ),
+          ],
+        );
 }
