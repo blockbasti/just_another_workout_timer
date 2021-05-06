6581290c98db99ac664a6a657f209e50a9315a5e
Bastian Block <blockbasti99@gmail.com>
2021-05-06 15:44:04 +0200

    feat: update sounds

--- lib/sound_helper.dart
+++ lib/sound_helper.dart
@@ -5,37 +5,43 @@ import 'package:soundpool/soundpool.dart';
 // ignore: avoid_classes_with_only_static_members
 class SoundHelper {
   static Soundpool _soundpool;
-  static int _beepId;
+  static int _beepLowId;
+  static int _beepHighId;
+  static int _tickId;
   static bool useSound = false;
 
   static Future<void> loadSounds() async {
-    _soundpool ??= Soundpool(streamType: StreamType.notification);
-    _beepId = await _loadSound();
+    _soundpool ??= Soundpool(streamType: StreamType.music);
+    await _loadSounds();
     useSound = PrefService.getString('sound') == 'beep' ?? true;
   }
 
-  static Future<int> _loadSound() async {
-    var asset = await rootBundle.load('assets/beep.wav');
-    return await _soundpool.load(asset);
+  static Future<void> _loadSounds() async {
+    var beepLow = await rootBundle.load('assets/beep_low.wav');
+    var beepHigh = await rootBundle.load('assets/beep_high.wav');
+    var tick = await rootBundle.load('assets/tick.wav');
+    _beepLowId = await _soundpool.load(beepLow);
+    _beepHighId = await _soundpool.load(beepHigh);
+    _tickId = await _soundpool.load(tick);
   }
 
   static void playBeepLow() {
-    if (useSound) _soundpool.play(_beepId);
+    if (useSound) _soundpool.play(_beepLowId);
   }
 
   static void playBeepHigh() {
-    if (useSound) _soundpool.play(_beepId, rate: 1.5);
+    if (useSound) _soundpool.play(_beepHighId);
   }
 
   static void playBeepTick() {
-    if (PrefService.getBool('ticks')) _soundpool.play(_beepId, rate: 2);
+    if (PrefService.getBool('ticks')) _soundpool.play(_tickId);
   }
 
   static void playDouble() {
     if (useSound) {
-      _soundpool.play(_beepId);
+      _soundpool.play(_beepLowId);
       Future.delayed(Duration(milliseconds: 200))
-          .then((value) => _soundpool.play(_beepId));
+          .then((value) => _soundpool.play(_beepLowId));
     }
   }
 }
