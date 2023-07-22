d97270a1428bea36d539b2b8ed5030c0bb5c576a
Bastian Block <git@bblock.dev>
2023-07-22 11:14:26 +0200

    chore: fix oss license generation

--- scripts/generate_code.sh
+++ scripts/generate_code.sh
@@ -1,5 +1,5 @@
 echo 'Generate license data'
-dart run flutter_oss_licenses:generate.dart
+flutter pub run flutter_oss_licenses:generate.dart
 
 echo 'Generate translations'
 dart run intl_utils:generate
